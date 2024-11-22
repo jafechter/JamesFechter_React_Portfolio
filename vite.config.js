f587412c0c5a09b00c4acae054ee9c2699df8bd5
James Fechter <jafechter25@gmail.com>
2024-11-22 13:48:59 -0500

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.JPG", "**/*.PNG"],
+  base: "/JamesFechter_React_Portfolio",
 });
